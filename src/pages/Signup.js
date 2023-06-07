5d0b8544820b9cbabb4ca1b1117d0033a69e8734
bohongu <jaehong2782@naver.com>
2023-06-07 13:30:46 +0900

    feat: signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,84 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const BASE_URL = "https://www.pre-onboarding-selection-task.shop";
 
 function Signup() {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(true);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = inputs;
+
+  const onChange = (event) => {
+    const { value, name } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { status } = await axios.post(
+        `${BASE_URL}/auth/signup`,
+        {
+          email,
+          password,
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      if (status === 201) {
+        navigate("/signin");
+      } else {
+        alert("회원가입이 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/todo");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    const isEmailValid = email.includes("@");
+    const isPasswordValid = password.length >= 8;
+    setDisabled(!(isEmailValid && isPasswordValid));
+  }, [email, password]);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <button data-testid="signup-button" disabled={disabled}>
+          회원가입
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
